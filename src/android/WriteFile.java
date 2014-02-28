2f143d33c3906a8791b0f0638e895bd99a924853
dejavusv <surachai.ns@gmail.com>
2014-02-28 13:11:42 +0700

    Update WriteFile.java

--- src/android/WriteFile.java
+++ src/android/WriteFile.java
@@ -29,11 +29,11 @@ public class WriteFile {
 			out.write(data + "\r\n");
 			out.write("");
 			out.close();
-
+			return ""+logfile.exists();
 		} catch (IOException e) {
 			Log.e("Exception", "File write failed: " + e.toString());
 		}
-		return ""+logfile.exists();
+		return "";
 	}
 
 }
